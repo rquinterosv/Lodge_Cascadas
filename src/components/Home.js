de7879a7ea90528f4976e01cf0a2182a64c4701b
rquinterosv <rafael.quinterosv@gmail.com>
2023-03-03 16:15:35 -0300

    Adding CSS font and colors

---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import Button from '@material-ui/core/Button';
+
+class Home extends Component {
+    render(){
+        return(
+            <><h1>Holaa</h1>
+            <Button variant="contained" color="primary">
+                Click me
+            </Button></>
+        )
+    }
+}
+
+export default Home; 
