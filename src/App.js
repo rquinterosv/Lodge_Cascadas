1ebbda93ac13ba952036feccddb4e5279cd3b2d7
rquinterosv <rafael.quinterosv@gmail.com>
2023-03-03 17:51:15 -0300

    Task1-first-draft-menu

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home'
 import About from './components/About'
-import Navbar from './components/Navbar';
+import Menu from './components/Menu';
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/components/Menu.js
@@ -0,0 +1,32 @@
+import React, {Component} from "react";
+import { NavLink } from "react-router-dom";
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Link from '@material-ui/core/Link';
+import logo from '../img/logo.png'
+
+class Menu extends Component {
+    render(){
+        return(
+            <div>
+                <AppBar class="menu" position="static">
+                    <Toolbar className="items">
+                        <Box className="image">
+                            <img className="my-image" src={logo} alt="LogoLC" />
+                        </Box>
+                    <Typography variant="h6">
+                        <Box className="options">
+                            <NavLink className="option" to="/"  exact>Home</NavLink>
+                            <NavLink className="option" to="/about" >About</NavLink>
+                        </Box>
+                    </Typography>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        )
+    }
+}
+
+export default Menu; 
